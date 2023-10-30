bdf3b89583728cc927add2b2ef96d70bf81329f3
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-10-29 20:59:44 -0700

    update the logic of TICTACTOE

--- logic.py
+++ logic.py
@@ -2,7 +2,6 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
 def make_empty_board():
     return [
         [None, None, None],
@@ -10,13 +9,27 @@ def make_empty_board():
         [None, None, None],
     ]
 
+def other_player(player):
+    if player == 'X':
+        return 'O'
+    else:
+        return 'X'
 
 def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    return None  # FIXME
+    # Check rows
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]
 
+    # Check columns
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] is not None:
+            return board[0][col]
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+
+    return None
